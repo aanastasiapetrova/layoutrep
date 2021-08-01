0f7eae4229e54a97f23f3d47bc4d2ffe120ac013
Anastasia Petrova <71938351+aanastasiapetrova@users.noreply.github.com>
2021-08-02 01:10:54 +0400

    Update index.php

--- index.php
+++ index.php
@@ -21,5 +21,5 @@ $qtsms = new Qtsms($cfg['login'], $cfg['password'], $cfg['host']);
 $result = $qtsms->post_message($sms_text, $phone_num, $phone_num, 'x124127456', $iwix_sms_period);
 
 header("Content-Type: text/xml; charset=UTF-8");
-echo $result;
+echo "hello";
 
