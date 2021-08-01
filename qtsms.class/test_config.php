acb427245f23285ebe60aa4a8677240a67b7a77f
Anastasia Petrova <71938351+aanastasiapetrova@users.noreply.github.com>
2021-08-02 00:19:45 +0400

    Add files via upload

---
+++ qtsms.class/test_config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Скрипт с общей конфигурацией для тестовых скриптов QTSMS.class.php
+ *
+ */
+
+$cfg = array(
+	'login' => 'aanastasiapetrova',					// ваш логин в системе
+	'password' => 'vkontakte1108',				// ваш пароль
+	'host' => 'https://aanastasiapetrova.github.io/'		// хост для доступа к сервису
+);
