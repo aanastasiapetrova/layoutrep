2c5840ec974fdf3ddd5b361610f1f5a19ab3739e
Anastasia Petrova <71938351+aanastasiapetrova@users.noreply.github.com>
2021-08-02 01:33:03 +0400

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $iwix_sms_period = 600;
 $qtsms = new Qtsms($cfg['login'], $cfg['password'], $cfg['host']);
 $result = $qtsms->post_message($sms_text, $phone_num, $phone_num, 'x124127456', $iwix_sms_period);
 
-header("Content-Type: text/xml; charset=UTF-8");
+//header("Content-Type: text/xml; charset=UTF-8");
 echo "hello";
 ?>
 
